36fc8aea53fbd2b05a9f1218971c28f5bc1ee401
Matheus Cunha <devmatheusgr@gmail.com>
2023-04-02 21:58:00 +0100

    feat: add config react query and tawilndcss

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 module.exports = {
   mode: "jit",
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  darkMode: false, // or 'media' or 'class'
+  purge: ["./pages/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  darkMode: false, // ou 'media' ou 'class'
   theme: {
     extend: {},
   },
