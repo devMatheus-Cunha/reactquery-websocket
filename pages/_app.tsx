9813447a96684c0d059d53cde18139e5d21f243c
Matheus Cunha <devmatheusgr@gmail.com>
2023-04-02 14:40:20 +0100

    feat: add react quey axios e tailwindcss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import './index.css';
+import 'tailwindcss/dist/base.css';
+import 'tailwindcss/dist/components.css';
+import 'tailwindcss/dist/utilities.css';
+
+
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
