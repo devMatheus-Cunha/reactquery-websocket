38cf515140c2dab50184132e25a713c4749e72a8
Matheus Cunha <devmatheusgr@gmail.com>
2023-04-23 21:14:16 +0100

    feat: configure react query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,8 +6,18 @@ import { Hydrate } from 'react-query/hydration';
 import { ReactQueryDevtools } from 'react-query/devtools';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [queryClient] = useState(() => new QueryClient());
-
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+        cacheTime: 1000 * 60 * 10, // cache data for 10 minutes
+        staleTime: 1000 * 60 * 5, // consider data stale after 5 minutes
+        // refetchInterval: 1000 * 60 * 15, // refetch data every 15 minutes
+        refetchOnMount: true, // refetch data when component first mounts
+        retry: false // Disable automatic refetch attempt
+      },
+    },
+  });
   return (
     <QueryClientProvider client={queryClient}>
       <Hydrate state={pageProps.dehydratedState}>
